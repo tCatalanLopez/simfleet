01747624ceaa4dcfdc3e0915867d60fcedc2c92e
Tomás <tocalo@etsii.upv.es>
2023-01-11 19:55:40 +0100

    arreglado movable, new_transport no carga

--- simfleet/movable.py
+++ simfleet/movable.py
@@ -8,4 +8,85 @@ ONESECOND_IN_MS = 1000
 
 class MovableMixin():
 
-    pass
+    def __init__(self):
+        self.set("path", None)
+        self.chunked_path = None
+        self.animation_speed = ONESECOND_IN_MS
+        self.set("speed_in_kmh", 3000)
+        
+        self.distances = []
+        self.durations = []
+
+    async def move_to(self, dest):
+        """
+        Moves the transport to a new destination.
+
+        Args:
+            dest (list): the coordinates of the new destination (in lon, lat format)
+
+        Raises:
+             AlreadyInDestination: if the transport is already in the destination coordinates.
+        """
+        if self.get("current_pos") == dest:
+            raise AlreadyInDestination
+        counter = 5
+        path = None
+        distance, duration = 0, 0
+        while counter > 0 and path is None:
+            logger.debug(
+                "Requesting path from {} to {}".format(self.get("current_pos"), dest)
+            )
+            path, distance, duration = await self.request_path(
+                self.get("current_pos"), dest
+            )
+            counter -= 1
+        if path is None:
+            raise PathRequestException("Error requesting route.")
+
+        self.set("path", path)
+        try:
+            self.chunked_path = chunk_path(path, self.get("speed_in_kmh"))
+        except Exception as e:
+            logger.error("Exception chunking path {}: {}".format(path, e))
+            raise PathRequestException
+        self.dest = dest
+        self.distances.append(distance)
+        self.durations.append(duration)
+        behav = self.MovingBehaviour(period=1)
+        self.add_behaviour(behav)
+
+    async def step(self):
+        """
+        Advances one step in the simulation
+        """
+        if self.chunked_path:
+            _next = self.chunked_path.pop(0)
+            distance = distance_in_meters(self.get_position(), _next)
+            self.animation_speed = (
+                distance / kmh_to_ms(self.get("speed_in_kmh")) * ONESECOND_IN_MS
+            )
+            await self.set_position(_next)
+
+
+    async def request_path(self, origin, destination):
+        """
+        Requests a path between two points (origin and destination) using the route server.
+
+        Args:
+            origin (list): the coordinates of the origin of the requested path
+            destination (list): the coordinates of the end of the requested path
+
+        Returns:
+            list, float, float: A list of points that represent the path from origin to destination, the distance and
+            the estimated duration
+
+        Examples:
+            >>> path, distance, duration = await self.request_path(origin=[0,0], destination=[1,1])
+            >>> print(path)
+            [[0,0], [0,1], [1,1]]
+            >>> print(distance)
+            2.0
+            >>> print(duration)
+            3.24
+        """
+        return await request_path(self, origin, destination, self.route_host)
--- simfleet/vehicle.py
+++ simfleet/vehicle.py
@@ -54,19 +54,12 @@ ONESECOND_IN_MS = 1000
 # esto realmente excepto toda la funcionalidad del cliente es vehicle
 class Vehicle(MovableMixin,GeoLocatedAgent):
     def __init__(self, agentjid, password):
-        super().__init__(agentjid, password)
-        self.set("path", None)
-        self.set("speed_in_kmh", 3000)
+        GeoLocatedAgent.__init__(self,agentjid, password)
+        MovableMixin.__init__(self)
+        
         self.fleetmanager_id = None
         self.registration = None
 
-        # a movable
-        self.chunked_path = None
-        self.animation_speed = ONESECOND_IN_MS
-        
-        self.distances = []
-        self.durations = []
-        
     async def setup(self):
         try:
             template = Template()
@@ -162,31 +155,7 @@ class Vehicle(MovableMixin,GeoLocatedAgent):
         #         if performative == ACCEPT_PERFORMATIVE:
         #             await self.begin_charging()
 
-    # movable mixin
-    async def request_path(self, origin, destination):
-        """
-        Requests a path between two points (origin and destination) using the route server.
-
-        Args:
-            origin (list): the coordinates of the origin of the requested path
-            destination (list): the coordinates of the end of the requested path
-
-        Returns:
-            list, float, float: A list of points that represent the path from origin to destination, the distance and
-            the estimated duration
-
-        Examples:
-            >>> path, distance, duration = await self.request_path(origin=[0,0], destination=[1,1])
-            >>> print(path)
-            [[0,0], [0,1], [1,1]]
-            >>> print(distance)
-            2.0
-            >>> print(duration)
-            3.24
-        """
-        return await request_path(self, origin, destination, self.route_host)
-
-    # se usa el metodo de geolocated, aparte de la funcionalidad de avisar al customer, que es algo solo del transporte
+    # se usa el metodo de geolocated, la funcion original de transport se divide entre vehicle y transport
     async def set_position(self, coords=None):
         """
         Sets the position of the transport. If no position is provided it is located in a random position.
@@ -281,81 +250,6 @@ class Vehicle(MovableMixin,GeoLocatedAgent):
             "fleet_manager": self.fleetmanager_id
         })
         return data
-
-    # todo esto va a movable mixing tal cual
-    async def move_to(self, dest):
-        """
-        Moves the transport to a new destination.
-
-        Args:
-            dest (list): the coordinates of the new destination (in lon, lat format)
-
-        Raises:
-             AlreadyInDestination: if the transport is already in the destination coordinates.
-        """
-        if self.get("current_pos") == dest:
-            raise AlreadyInDestination
-        counter = 5
-        path = None
-        distance, duration = 0, 0
-        while counter > 0 and path is None:
-            logger.debug(
-                "Requesting path from {} to {}".format(self.get("current_pos"), dest)
-            )
-            path, distance, duration = await self.request_path(
-                self.get("current_pos"), dest
-            )
-            counter -= 1
-        if path is None:
-            raise PathRequestException("Error requesting route.")
-
-        self.set("path", path)
-        try:
-            self.chunked_path = chunk_path(path, self.get("speed_in_kmh"))
-        except Exception as e:
-            logger.error("Exception chunking path {}: {}".format(path, e))
-            raise PathRequestException
-        self.dest = dest
-        self.distances.append(distance)
-        self.durations.append(duration)
-        behav = self.MovingBehaviour(period=1)
-        self.add_behaviour(behav)
-
-    async def step(self):
-        """
-        Advances one step in the simulation
-        """
-        if self.chunked_path:
-            _next = self.chunked_path.pop(0)
-            distance = distance_in_meters(self.get_position(), _next)
-            self.animation_speed = (
-                distance / kmh_to_ms(self.get("speed_in_kmh")) * ONESECOND_IN_MS
-            )
-            await self.set_position(_next)
-
-
-    async def request_path(self, origin, destination):
-        """
-        Requests a path between two points (origin and destination) using the route server.
-
-        Args:
-            origin (list): the coordinates of the origin of the requested path
-            destination (list): the coordinates of the end of the requested path
-
-        Returns:
-            list, float, float: A list of points that represent the path from origin to destination, the distance and
-            the estimated duration
-
-        Examples:
-            >>> path, distance, duration = await self.request_path(origin=[0,0], destination=[1,1])
-            >>> print(path)
-            [[0,0], [0,1], [1,1]]
-            >>> print(distance)
-            2.0
-            >>> print(duration)
-            3.24
-        """
-        return await request_path(self, origin, destination, self.route_host)
 
     class MovingBehaviour(PeriodicBehaviour):
             """
